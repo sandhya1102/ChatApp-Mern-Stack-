52d128528fd83f8fc9ebe36a68565c28daa8c10f
sandhya1102 <sandhay165@gmail.com>
2025-06-19 15:59:26 +0530

    update for deploy

--- backend/index.js
+++ backend/index.js
@@ -1,40 +1,43 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
-import userRoute from "./routes/userRoute.js"
-import messageRoute from "./routes/messageRoute.js"
+import userRoute from "./routes/userRoute.js";
+import messageRoute from "./routes/messageRoute.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import {app,server} from "./socket/socket.js"
+import { app, server } from "./socket/socket.js";
 import path from "path";
+import serverless from 'serverless-http'
 dotenv.config({});
-
 
 const PORT = process.env.PORT || 5000;
 
 const _dirname = path.resolve();
 
 //middleware
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
-const corsOption={
-    origin:"https://chatapp-mern-stack-1.onrender.com/",
-    credentials:true
-}
+const corsOption = {
+  origin: "https://chatapp-mern-stack-1.vercel.app",
+  methods: ["POST", "GET"],
+  credentials: true,
+};
 
 app.use(cors(corsOption));
 
-app.use("/api/v1/user",userRoute);
-app.use("/api/v1/message" , messageRoute);
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/message", messageRoute);
 
 // serve frontend
-app.use(express.static(path.join(_dirname, "/frontend/dist")));
+// app.use(express.static(path.join(_dirname, "/frontend/dist")));
 
-app.get(/(.*)/, (_, res) => {
-    res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+// app.get(/(.*)/, (_, res) => {
+//   res.sendFile(path.resolve(_dirname, "frontend", "dist", "index.html"));
+// });
+
+server.listen(PORT, () => {
+  connectDB();
 });
 
-server.listen(PORT,()=>{
-    connectDB();
-})
+export default serverless(app)
